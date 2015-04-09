a1334b716ab533bab1447f72f1c03d0529ca1db6
Alexandra Qin <alexandraqin@gmail.com>
2015-04-09 17:31:11 -0400

    Add printHelloWorld to main

--- src/nyc/c4q/Unit0Tests.java
+++ src/nyc/c4q/Unit0Tests.java
@@ -13,6 +13,8 @@ public class Unit0Tests {
      */
     public static void main (String args[]) {
       // Use main to test your methods
+
+      printHelloWorld();
     }
 
     public static void printHelloWorld() {
